3b86e34229fbf827f4de4bf250daa00c7c488f88
Ramanathan984 <70475167+Ramanathan984@users.noreply.github.com>
2024-08-26 16:51:26 +0530

    conditional and dynamic forms

--- HelloApp/src/app/app.component.ts
+++ HelloApp/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { state } from '@angular/animations';
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup ,FormArray } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { forbiddenNameValidator } from './shared/forbiddenName.validator';
 import { passwordValidator } from './shared/password.validator';
@@ -10,14 +10,90 @@ import { passwordValidator } from './shared/password.validator';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'HelloApp'; 
 
+  registrationForm : FormGroup;
 
+  public contactTypes = ["primary","secondary","emergency"]
 
+  
   get userName() {
-         return this.registrationForm.get("userName");
+    return this.registrationForm.get("userName");
   }
+
+  get subscribe() {
+    return this.registrationForm.get("subscribe");
+  }
+
+get email() {
+ return this.registrationForm.get("email");
+}
+
+get alternateEmails() {
+  return this.registrationForm.get("alternateEmails") as FormArray;
+}
+
+get contact(){
+  return this.registrationForm.get('contact') as FormArray;
+}
+
+addContact(){
+ this.contact.push(this._formBuilder.group({
+     contactType:[''],
+     mobileNumber:['']
+
+ }));
+}
+
+onRemove(i){
+  this.contact.removeAt(i);
+}
+
+
+  //add the alternate emails input
+  addAlterNateEmails(){
+    console.log("adding alternate emails ...");
+    this.alternateEmails.push(this._formBuilder.control(''));
+  }
+  ngOnInit(): void {
+
+      // by using form builder service 
+      this.registrationForm = this._formBuilder.group({
+        userName : ['',[Validators.required,Validators.minLength(3),forbiddenNameValidator(/Password/)]],
+          password : [''],
+          confirmPassword : [''],
+          email :[''],
+          alternateEmails:this._formBuilder.array([]),
+          subscribe : [false],
+          address : this._formBuilder.group({
+            city : [''],
+            state : [''],
+            country : ['']  
+          }),
+          contact : this._formBuilder.array([this._formBuilder.group({
+            contactType : [''],
+            mobileNumber  :['']  
+          })])
+         },{validator : passwordValidator});     
+
+
+
+         //conditional validation
+         this.subscribe.valueChanges.subscribe(checkedValue =>{
+
+          const email = this.email;
+          if (checkedValue) {
+            email.setValidators(Validators.required);
+          } else {
+            email.clearValidators();
+          }
+
+             email.updateValueAndValidity();
+         });
+    
+  }
+
 
   constructor(private _formBuilder:FormBuilder){}
 
@@ -33,19 +109,7 @@ export class AppComponent {
   //    })
   //   });
 
-    // by using form builder service 
-    public registrationForm = this._formBuilder.group({
-   userName : ['',[Validators.required,Validators.minLength(3),forbiddenNameValidator(/Password/)]],
-     password : [''],
-     confirmPassword : [''],
-     address : this._formBuilder.group({
-       city : [''],
-       state : [''],
-       country : ['']  
-     })
-
-
-    },{validator : passwordValidator})     
+  
 
 
 
